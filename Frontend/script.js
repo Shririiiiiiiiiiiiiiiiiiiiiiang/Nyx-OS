d2c202198a8b448f5c73ef487845c000a426cc38
Shrirang Saraogi <saraogishrirang@gmail.com>
2026-06-01 17:48:25 +0530

    Added typing effect(letters come one by one) and custom Nyx replies to specific inputs such as hi/who are you ...

--- Frontend/script.js
+++ Frontend/script.js
@@ -6,10 +6,44 @@ button.addEventListener("click", function () {
 
     const message = input.value;
 
-    chatBox.innerHTML += "<p>You: " + message + "</p>";
+    chatBox.innerHTML += '<div class="message userMessage">You: ' + message + '</div>';
 
-    chatBox.innerHTML += "<p>Nyx; I am Online. </p>";
+    let reply = "i am still learning.";
+    if(message.toLowerCase() === "hi"){
+        reply = "Hello, I am Nyx";
+    }  
+    if(message.toLowerCase() === "who are you") {
+        reply = "I am your AI Assistant.";
+    }
+    if(message.toLowerCase() === "help") {
+        reply = "I can help you with files,coding ,sorting & projects.";
+    }
+    if(message.toLowerCase() === "hello") {
+        reply = "Hello There.";
+    }
+    if(message.toLowerCase() === "creator") {
+        reply = "Secret Buddy (AKA. Shrirang)";
+    }
 
+    const nyxBubble = document.createElement("div");
+    nyxBubble.className = "message nyxMessage";
+    chatBox.appendChild(nyxBubble);
+
+    let i = 0;
+
+    const typingEffect = setInterval(function () {
+
+        nyxBubble.innerHTML = 
+            "Nyx: " + reply.substring(0,i);
+
+        i++;
+        
+        if(i > reply.length){
+
+            clearInterval(typingEffect);
+
+        }
+    }, 50);
     input.value = "";
 
 });
