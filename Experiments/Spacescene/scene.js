433bf5ffd654b4497082c744c67c8c4ba9c5b6b5
Shrirang Saraogi <saraogishrirang@gmail.com>
2026-07-14 17:19:05 +0530

    made moon but the craters are bad also fixed moon 2 to 3 times as it was forming wrongly

--- Experiments/Spacescene/scene.js
+++ Experiments/Spacescene/scene.js
@@ -1,5 +1,44 @@
 import * as THREE from 'three';
 
+const moonCanvas = document.createElement("canvas");
+moonCanvas.width = 512;
+moonCanvas.height = 512;
+const ctx = moonCanvas.getContext("2d");
+
+
+ctx.beginPath();
+ctx.arc(
+    256, 256, 180, 0, Math.PI * 2
+);
+const moonGradient = ctx.createRadialGradient(
+    220, 200, 20, 256, 256, 180
+);
+moonGradient.addColorStop(0, "#f0f0f0");
+moonGradient.addColorStop(1, "#b8b8b8");
+ctx.fillStyle = moonGradient;
+ctx.fill();
+
+for(let i = 0; i < 20; i++) {  //moon craterss
+    const angle = Math.random() * Math.PI * 2;
+    const distance = Math.random() * 110;
+    const x = 256 + Math.cos(angle) * distance
+    const y = 256 + Math.sin(angle) * distance
+    
+    const r =  
+    8 + Math.random() * 20;
+
+    ctx.beginPath();
+    ctx.arc(
+        x, y, r, 0, Math.PI * 2
+    );
+    ctx.fillStyle = "rgba(90, 90, 90, 0.4)";
+    ctx.fill();
+}
+
+const moonTexture = new THREE.CanvasTexture(
+    moonCanvas
+);
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
@@ -31,7 +70,7 @@ window.addEventListener(
     "resize",
     () => {
         camera.aspect = window.innerWidth/window.innerHeight;
-        camera.updateMatrix.ProjectionMatrix();
+        camera.updateProjectionMatrix();
         renderer.setSize(
             window.innerWidth,
             window.innerHeight
@@ -202,33 +241,18 @@ const mediumStars =
     scene.add(mediumStars);
 
 
-const moonGeometry = new THREE.SphereGeometry(
-        5,
-        64,
-        64
-    );
-const moonMaterial = new THREE.MeshPhongMaterial({
-    color: 0xe0e0e0
+const moonMaterial = new THREE.SpriteMaterial({
+    map: moonTexture,
+    transparent: true
 });
-const moon  = new THREE.Mesh(
-    moonGeometry,
+const moon = new THREE.Sprite(
     moonMaterial
 );
-
-moon.position.set(
-    25, 15, -40
-);
-
-
-
+moon.position.set( 18, 10, -20);
+moon.scale.set(12, 12, 1);
 scene.add(moon);
-const moonLight= new THREE.DirectionalLight(
-    0xffffff, 2
-);
 
-moonLight.position.set( 20, 20, 20
-);
-scene.add(moonLight);
+
 
 
     
