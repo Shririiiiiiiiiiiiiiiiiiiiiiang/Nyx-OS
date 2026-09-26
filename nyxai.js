2c87826d526be76b3bead0ffc24159e1a4f3943b
Shrirang Saraogi <saraogishrirang@gmail.com>
2026-09-26 23:55:03 +0530

    added a popup if the browser doesnt support web gpu or for some reasn a i doesnt load

--- nyxai.js
+++ nyxai.js
@@ -1,14 +1,46 @@
 import {CreateMLCEngine} from "@mlc-ai/web-llm";
 let engine = null;
+window.nyxAIReady = false;
+
+function showAiworksyn(text) {
+    const note = document.getElementById("aicheck");
+    note.textContent = text;
+    note.classList.remove("hidden");
+    setTimeout(function() {
+        note.classList.add("hidden");
+    }, 5000);
+}
+
+
+
+
+
+
+
+
+
 
 async function loadEngine() {
+    if(!navigator.gpu) {
+        console.log("webgpu not supported for this thing please use chrome maybe");
+        showAiworksyn("The browser currently running doesnt let the Ai upload or be in use Please use chrome as it will mostly workk");
+        return;
+    }
+    try {
     engine = await CreateMLCEngine("Qwen2.5-1.5B-Instruct-q4f16_1-MLC", {
         initProgressCallback: function(progress) {
             console.log("loadingggg:", progress.text);
         }
     });
+    window.nyxAIReady = true;
     console.log("The Nyx AI is hererererere");
-}
+    }
+    catch (error) {
+        console.log("Ai didnt loadddd", error);
+        showAiworksyn("Ai isnt loading try again or use basic replies hardcoded oness")
+
+    }
+}    
 
 loadEngine();
 
